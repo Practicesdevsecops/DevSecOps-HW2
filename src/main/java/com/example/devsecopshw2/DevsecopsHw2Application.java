f422f00251fc69f4d8c46414b17840a0eca975fc
zaidenx <a0925500592@gmail.com>
2026-06-08 20:01:16 +0800

    Create Spring Boot application and automated tests

---
+++ src/main/java/com/example/devsecopshw2/DevsecopsHw2Application.java
@@ -0,0 +1,13 @@
+package com.example.devsecopshw2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsecopsHw2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsecopsHw2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devsecopshw2/DevsecopsHw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devsecopshw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsecopsHw2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
